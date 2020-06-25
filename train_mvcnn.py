a4d3524a97e11070fdba4d91702e5d078a7032d6
sngver <67322452+sngver@users.noreply.github.com>
2020-06-25 18:50:16 +0100

    Update train_mvcnn.py

--- train_mvcnn.py
+++ train_mvcnn.py
@@ -21,6 +21,7 @@ parser.add_argument("-num_views", type=int, help="number of views", default=12)
 parser.add_argument("-train_path", type=str, default="modelnet40_images_new_12x/*/train")
 parser.add_argument("-val_path", type=str, default="modelnet40_images_new_12x/*/test")
 parser.set_defaults(train=False)
+parser.add_argument("-epoch","--epoch",type=int,help="number of epochs",default=30)
 
 def create_folder(log_dir):
     # make summary folder
@@ -58,7 +59,7 @@ if __name__ == '__main__':
     print('num_train_files: '+str(len(train_dataset.filepaths)))
     print('num_val_files: '+str(len(val_dataset.filepaths)))
     trainer = ModelNetTrainer(cnet, train_loader, val_loader, optimizer, nn.CrossEntropyLoss(), 'svcnn', log_dir, num_views=1)
-    trainer.train(30)
+    trainer.train(args.epoch)
 
     # STAGE 2
     log_dir = args.name+'_stage_2'
@@ -76,6 +77,6 @@ if __name__ == '__main__':
     print('num_train_files: '+str(len(train_dataset.filepaths)))
     print('num_val_files: '+str(len(val_dataset.filepaths)))
     trainer = ModelNetTrainer(cnet_2, train_loader, val_loader, optimizer, nn.CrossEntropyLoss(), 'mvcnn', log_dir, num_views=args.num_views)
-    trainer.train(30)
+    trainer.train(args.epoch)
 
 
